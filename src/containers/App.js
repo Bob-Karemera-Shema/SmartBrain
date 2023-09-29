49838346dd4fac0bf1f0992a181abe5fcd36fa3e
Bob-Karemera-Shema <72112797+Bob-Karemera-Shema@users.noreply.github.com>
2023-09-29 13:07:06 +0100

    Particles background and rank statement

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import Navigation from '../components/Navigation';
 import Logo from '../components/Logo';
 import ImageLinkForm from '../components/ImageLinkForm';
+import Rank from '../components/Rank';
+import ParticlesContainer from '../components/Particles';
 
 function App() {
   return (
     <div className="App">
+      <ParticlesContainer className="particles" />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
