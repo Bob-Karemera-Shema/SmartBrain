929e0053a7a66582704a7920a84e4096b3e6e357
Bob-Karemera-Shema <72112797+Bob-Karemera-Shema@users.noreply.github.com>
2023-10-09 15:44:16 +0100

    FaceRecognition Component source code edit

--- src/components/FaceRecognition/index.js
+++ src/components/FaceRecognition/index.js
@@ -4,7 +4,7 @@ export default function FaceRecognition({ box, imageURL }) {
     return (
         <div className="center ma">
             <div className="absolute mt2">
-                <img id="input-image" src={imageURL} alt="input" width='500px' height='auto' />
+                <img id="input-image" src={imageURL} alt="" width='500px' height='auto' />
                 <div
                     className="bounding-box"
                     style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}
