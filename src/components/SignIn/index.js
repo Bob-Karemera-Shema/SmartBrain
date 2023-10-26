46e85d5f225d7d82eb824a32416730796b30f442
Bob-Karemera-Shema <72112797+Bob-Karemera-Shema@users.noreply.github.com>
2023-10-26 15:32:56 +0100

    Sign In requests to server (1)

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,4 +1,34 @@
+import { useState } from "react";
+
 export default function SignIn({ onRouteChange }) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const onPasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const onSubmitSignin = () => {
+        fetch("http://localhost:3030/signin", {
+            method: 'post',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data === "success") {
+                onRouteChange("home");
+            }
+        })
+    };
+
     return (
         <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
             <main className="pa4 black-80">
@@ -7,11 +37,23 @@ export default function SignIn({ onRouteChange }) {
                         <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                         <div className="mt3">
                             <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
+                            <input 
+                                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                type="email" 
+                                name="email-address" 
+                                id="email-address" 
+                                onChange={onEmailChange}
+                            />
                         </div>
                         <div className="mv3">
                             <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
+                            <input 
+                                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                type="password" 
+                                name="password" 
+                                id="password" 
+                                onChange={onPasswordChange}
+                            />
                         </div>
                     </fieldset>
                     <div className="">
@@ -19,7 +61,7 @@ export default function SignIn({ onRouteChange }) {
                             className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                             type="submit"
                             value="Sign in"
-                            onClick={() => onRouteChange("home")}
+                            onClick={onSubmitSignin}
                         />
                     </div>
                     <div className="lh-copy mt3">
